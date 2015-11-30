e9240b963a35bf21573e87121e7411c4bdaeabcf
encorehu <huyoo353@126.com>
2015-11-30 20:56:27 +0800

    modify admin

--- crm/mybill/admin.py
+++ crm/mybill/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Account
+from .models import AccountItem
+
+class AccountAdmin(admin.ModelAdmin):
+	pass
+
+class AccountItemAdmin(admin.ModelAdmin):
+	list_display=('tx_date','debit','credit')
+
+admin.site.register(Account, AccountAdmin)
+admin.site.register(AccountItem, AccountItemAdmin)
