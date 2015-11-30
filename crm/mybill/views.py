3849e41fd0e7c8aa534991d5f7b7eac7343f8034
encorehu <huyoo353@126.com>
2015-11-30 21:02:45 +0800

    add BillIndexView

--- crm/mybill/views.py
+++ crm/mybill/views.py
@@ -1,3 +1,11 @@
+#-*- coding=utf-8 -*-
 from django.shortcuts import render
 
+from django.views.generic import ListView
+
 # Create your views here.
+class BillIndexView(ListView):
+    template_name = 'mybill/index.html'
+
+    def get_queryset(self):
+        return []
