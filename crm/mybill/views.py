44ef27b5cea1aa11b0114b258db3ad3104eb7f93
encorehu <huyoo353@126.com>
2015-12-14 10:09:02 +0800

    update post  kwargs

--- crm/mybill/views.py
+++ crm/mybill/views.py
@@ -200,22 +200,27 @@ class BillDoView(ListView):
             account = Account.objects.get(id=accountid)
         except Account.DoesNotExist:
             raise Http404("Account does not exist")
+        account_list = Account.objects.all()
+        kwargs.update({
+            'account':account,
+            'account_list':account_list,
+        })
         method=request.POST.get('method', '')
         if not method:
             method = request.GET.get('method', 'list')
         self.template_name = 'mybill/%s.html' % method
         if method == 'addOrUpdate':
-            return self.addOrUpdate(request)
+            return self.addOrUpdate(request, *args, **kwargs)
         elif method == 'listmonth':
-            return self.listmonth(request)
+            return self.listmonth(request, *args, **kwargs)
         elif method == 'edit':
-            return self.edit(request)
+            return self.edit(request, *args, **kwargs)
         elif method == 'append':
-            return self.append(request)
+            return self.append(request, *args, **kwargs)
         elif method == 'list':
-            return self.listall(request)
+            return self.listall(request, *args, **kwargs)
         elif method == 'listsort':
-            return self.listsort(request)
+            return self.listsort(request, *args, **kwargs)
         else:
             return render(request, self.template_name, {'form': ''})
 
