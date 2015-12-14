0b5d8a4675517771e0e38e991c59601cf033b43a
encorehu <huyoo353@126.com>
2015-12-14 20:47:34 +0800

    filter parent

--- crm/mybill/admin.py
+++ crm/mybill/admin.py
@@ -98,7 +98,7 @@ class AccountItemAdmin(admin.ModelAdmin):
 
 class AccountCategoryAdmin(admin.ModelAdmin):
     list_display=('account', 'tx_type','name')
-    list_filter=('account','name','tx_type')
+    list_filter=('account','parent','name','tx_type')
     actions = ['changeAccount']
     accountSuccess = Template('{{ count }} link{{ count|pluralize }}`s account changed to {{ account.name }}')
 
