e3b4d9125334c951fa551b2efe6ce0cfaa2237c9
encorehu <huyoo353@126.com>
2015-12-14 10:51:28 +0800

    add change account admin action

--- crm/mybill/admin.py
+++ crm/mybill/admin.py
@@ -1,3 +1,4 @@
+#-*- coding=utf-8 -*-
 from django.contrib import admin
 
 from django.contrib import admin
@@ -20,8 +21,9 @@ class AccountAdmin(admin.ModelAdmin):
 class AccountItemAdmin(admin.ModelAdmin):
     list_display=('account','tx_date','category','summary','tx_type','amount')
     search_fields = ('summary',)
-    actions = ['changeCategory']
+    actions = ['changeCategory', 'changeAccount']
     categorySuccess = Template('{{ count }} link{{ count|pluralize }}`s category changed to {{ category.name }}')
+    accountSuccess = Template('{{ count }} link{{ count|pluralize }}`s account changed to {{ account.name }}')
 
     class CategoryForm(forms.Form):
         _selected_action = forms.CharField(widget=forms.MultipleHiddenInput)
@@ -51,6 +53,38 @@ class AccountItemAdmin(admin.ModelAdmin):
                                   {'links': queryset, 'form': form, 'path':request.get_full_path()},
                                   context_instance=RequestContext(request))
     changeCategory.short_description = 'change category'
+
+    class AccountForm(forms.Form):
+        _selected_action = forms.CharField(widget=forms.MultipleHiddenInput)
+        account = forms.ModelChoiceField(Account.objects)
+
+    def changeAccount(self, request, queryset):
+        form = None
+        print request.POST
+        if 'cancel' in request.POST:
+            self.message_user(request, 'Canceled link account')
+            return
+        elif 'categorize' in request.POST:
+            #do the categorization
+            form = self.AccountForm(request.POST)
+            if form.is_valid():
+                account = form.cleaned_data['account']
+                queryset.update(account=account)
+            else:
+                print form.errors()
+                #for item in queryset:
+                #    item.account = account
+                #    item.save()
+            self.message_user(request, self.accountSuccess.render(Context({'count':queryset.count(), 'account':account})))
+            return HttpResponseRedirect(request.get_full_path())
+
+        if not form:
+            form = self.AccountForm(initial={'_selected_action': request.POST.getlist(admin.ACTION_CHECKBOX_NAME)})
+        return render_to_response('mybill/change_account.html',
+                                  {'links': queryset, 'form': form, 'path':request.get_full_path()},
+                                  context_instance=RequestContext(request))
+    changeAccount.short_description = u'change Account'
+
 
 class AccountCategoryAdmin(admin.ModelAdmin):
     list_display=('tx_type','name')
