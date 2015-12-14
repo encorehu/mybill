7741f1ab654e1966d57f952e35b7d0220ed8fff3
encorehu <huyoo353@126.com>
2015-12-14 09:01:29 +0800

    multi account

--- crm/mybill/urls.py
+++ crm/mybill/urls.py
@@ -4,7 +4,7 @@ from mybill.views import BillDoView
 from mybill.views import BillCategoryDoView
 
 urlpatterns = [
-    url(r'bill.do', BillDoView.as_view()),
-    url(r'category.do', BillCategoryDoView.as_view()),
-    url(r'$', BillIndexView.as_view()),
+    url(r'(?P<accountid>\d+)/bill.do', BillDoView.as_view()),
+    url(r'(?P<accountid>\d+)/category.do', BillCategoryDoView.as_view()),
+    url(r'(?P<accountid>\d+)/$', BillIndexView.as_view()),
 ]
