fe68ce0f4c70ac25253e154053f5df6cf736f07d
encorehu <huyoo353@126.com>
2016-01-04 22:29:25 +0800

    bill.do to book.do

--- crm/mybook/urls.py
+++ crm/mybook/urls.py
@@ -4,7 +4,7 @@ from mybook.views import BookDoView
 from mybook.views import BookCategoryDoView
 
 urlpatterns = [
-    url(r'(?P<bookid>\d+)/bill.do', BookDoView.as_view()),
+    url(r'(?P<bookid>\d+)/book.do', BookDoView.as_view()),
     url(r'(?P<bookid>\d+)/category.do', BookCategoryDoView.as_view()),
     url(r'(?P<bookid>\d+)/$', BookIndexView.as_view()),
     url(r'(?P<bookid>\d+)', BookIndexView.as_view()),
