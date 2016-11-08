a0ed32e0c57a1859ea4cacade9e7dbc487ecb223
encorehu <huyoo353@126.com>
2016-11-08 14:05:44 +0800

    export month like export year

--- crm/mybill/views.py
+++ crm/mybill/views.py
@@ -498,7 +498,8 @@ class BillDoView(ListView):
         i=0
         accountitem_list = AccountItem.objects.select_related('category').filter(account=account, tx_date__year=year_of_last_month, tx_date__month=month)
         for i, item  in enumerate(accountitem_list):
-            worksheet.write('A%s' % (i+start_row), unicode(item.category), format1)
+            category = item.category.name if item.category else ''
+            worksheet.write('A%s' % (i+start_row), u' %s %s' % ('+' if item.tx_type else '-', category), format1)
             worksheet.write('B%s' % (i+start_row), item.tx_date.strftime('%Y-%m-%d'), format1)
             worksheet.write('C%s' % (i+start_row), item.summary_display(), format1)
             if item.tx_type:
