03fcbc7e21f52d6798f63a6f9a3d8f93f5b8987e
encorehu <huyoo353@126.com>
2016-11-08 14:03:45 +0800

    one space

--- crm/mybill/views.py
+++ crm/mybill/views.py
@@ -626,7 +626,7 @@ class BillDoView(ListView):
         accountitem_list = AccountItem.objects.select_related('category').filter(account=account, tx_date__year=year)
         for i, item  in enumerate(accountitem_list):
             category = item.category.name if item.category else ''
-            worksheet.write('A%s' % (i+start_row), u'%s %s' % ('+' if item.tx_type else '-', category), format1)
+            worksheet.write('A%s' % (i+start_row), u' %s %s' % ('+' if item.tx_type else '-', category), format1)
             worksheet.write('B%s' % (i+start_row), item.tx_date.strftime('%Y-%m-%d'), format1)
             worksheet.write('C%s' % (i+start_row), item.summary_display(), format1)
             if item.tx_type:
