c9709d08e8a453caa42a99f44bcafb79547a55e7
encorehu <huyoo353@126.com>
2016-11-14 10:07:38 +0800

    fix last month balance

--- crm/mybill/views.py
+++ crm/mybill/views.py
@@ -479,7 +479,7 @@ class BillDoView(ListView):
           last_month = 12
         else:
           year_of_last_month = year
-        accountitem_list = AccountItem.objects.select_related('category').filter(account=account, tx_date__year=year, tx_date__lt=datetime.datetime(year,month,1))
+        accountitem_list = AccountItem.objects.select_related('category').filter(account=account,  tx_date__lt=datetime.datetime(year,month,1))
         #accountitem_list = AccountItem.objects.select_related('category').filter(tx_date__year=year_of_last_month, tx_date__month=month)
         last_balance = 0
         last_month_income = accountitem_list.filter(tx_type=1).aggregate(
