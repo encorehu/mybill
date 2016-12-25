c8ce973272fe62b577a27304bca58ff258fe0c4f
encorehu <huyoo353@126.com>
2016-12-25 21:09:31 +0800

    title gen header center

--- crm/mybill/views.py
+++ crm/mybill/views.py
@@ -677,8 +677,9 @@ class BillDoView(ListView):
             worksheet.write('E%s' % (i+start_row+1), total_outcome)
             worksheet.write('F%s' % (i+start_row+1), balance)
 
+        title = u'%s%s年日记账' % (account, year, )
         left = u'&L\n单位:%s' % settings.ORGNAME
-        center = u'&C%s日记账%s年' % (account, year, )
+        center = u'&C%s' % title
         right = '' #u'&R\n打印日期:%s' % datetime.datetime.now().strftime('%Y-%m-%d')
         worksheet.set_header(left+center+right, margin=0.6)
         worksheet.set_footer('&C&P/&N', margin=0.5)
@@ -688,7 +689,7 @@ class BillDoView(ListView):
         worksheet.print_area('A1:F1048576') #same as A:F
 
         workbook.set_properties({
-            'title':    u'%s%s年日记账' % (account, year, ),
+            'title':    title,
             'subject':  u'日记账',
             'author':   settings.AUTHOR,
             'manager':  settings.MANAGER,
